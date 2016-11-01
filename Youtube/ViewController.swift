661cfda19afc8d700ed84cf95f856a8686881d60
sophiakc <sophiakc@gmail.com>
2016-11-01 01:06:52 -0700

    W4 Lab Youtube init project

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Sophia KC on 01/11/2016.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
